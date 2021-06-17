122ba91c854760ac2f51d311f253126b3db18ac9
Toby-donghunseo <donghun.seo@sk.com>
2021-06-17 23:44:26 +0900

    Setting pylint

--- script/make_embedding_script.py
+++ script/make_embedding_script.py
@@ -1,8 +1,10 @@
-from pathlib import Path
-from test_qbh import QbhSystem
 import argparse
-from utils.data_utils import get_song_ids_of_selected_genre, song_id_to_pitch_txt_path
+from pathlib import Path
 import _pickle as pickle
+from test_qbh import QbhSystem
+from utils.data_utils import get_song_ids_of_selected_genre
+#from utils.data_utils import get_song_ids_of_selected_genre, song_id_to_pitch_txt_path
+
 
 
 if __name__ == '__main__':
@@ -10,18 +12,18 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output_directory', type=str,
                         default="emb_test",
                         help='directory to save embedding')
-    parser.add_argument('-c', '--checkpoint_directory', type=Path, 
+    parser.add_argument('-c', '--checkpoint_directory', type=Path,
                         default=Path("weights/contour_model"),
                         help='directory to load checkpoint')
-    parser.add_argument('--dataset_dir', type=Path, 
+    parser.add_argument('--dataset_dir', type=Path,
                         default= Path('/home/svcapp/t2meta/flo_new_music/music_100k/'),
-                        help='directory to load audio files (m4a, aac)') 
-    parser.add_argument('--meta_dat_path', type=str, 
+                        help='directory to load audio files (m4a, aac)')
+    parser.add_argument('--meta_dat_path', type=str,
                     default= 'data/flo_metadata_220k.dat',
                     help='directory to load metadata')
-    parser.add_argument('--humm_db_ids_path', type=str, 
+    parser.add_argument('--humm_db_ids_path', type=str,
                 default= 'data/humm_db_ids.dat',
-                help='directory to load humm_db_ids')       
+                help='directory to load humm_db_ids')
     parser.add_argument('--device', type=str, default='cuda',
                              help='cpu or cuda')
     args = parser.parse_args()
@@ -36,5 +38,9 @@ if __name__ == '__main__':
         humm_ids = pickle.load(f)
         # list of humming ids
     song_ids += humm_ids
-
-    qbh_system = QbhSystem(args.checkpoint_directory, args.output_directory, args.device, audio_dir=args.dataset_dir, make_emb=True, song_ids=song_ids)
+    qbh_system = QbhSystem(args.checkpoint_directory,
+                        args.output_directory,
+                        args.device,
+                        audio_dir=args.dataset_dir,
+                        make_emb=True,
+                        song_ids=song_ids)
--- script/make_melody_estimation.py
+++ script/make_melody_estimation.py
@@ -1,27 +1,24 @@
 from pathlib import Path
-from test_qbh import QbhSystem
 import argparse
+import _pickle as pickle
+#from test_qbh import QbhSystem
 from utils.data_utils import get_song_ids_of_selected_genre
 from utils.melody_utils import MelodyLoader
-import _pickle as pickle
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_dir', type=Path, 
+    parser.add_argument('--dataset_dir', type=Path,
                         default= Path('/home/svcapp/t2meta/flo_new_music/music_100k/'),
-                        help='directory to load checkpoint')     
-    parser.add_argument('--meta_dat_path', type=str, 
+                        help='directory to load checkpoint')
+    parser.add_argument('--meta_dat_path', type=str,
                         default= 'data/flo_metadata_220k.dat',
                         help='directory to load metadata')
-    parser.add_argument('--humm_db_ids_path', type=str, 
+    parser.add_argument('--humm_db_ids_path', type=str,
                 default= 'data/humm_db_ids.dat',
-                help='directory to load humm_db_ids')     
-      
+                help='directory to load humm_db_ids')
     parser.add_argument('--device', type=str, default='cuda',
                              help='cpu or cuda')
 
-                        
     args = parser.parse_args()
 
     with open(args.meta_dat_path, 'rb') as f:
@@ -34,4 +31,4 @@ if __name__ == '__main__':
     song_ids += humm_ids
 
     melody_loader = MelodyLoader(args.dataset_dir)
-    melody_loader.check_txt_exists(song_ids, make_txt=True, verbose=True) 
+    melody_loader.check_txt_exists(song_ids, make_txt=True, verbose=True)
